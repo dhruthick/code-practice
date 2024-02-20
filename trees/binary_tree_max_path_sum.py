6da5eb6acd129b311f7a27c30c17999a4e4e461e
Dhruthick Gowda <dhruthick1215.m@gmail.com>
2024-02-20 12:39:32 -0800

    two hard tree problems

---
+++ trees/binary_tree_max_path_sum.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    '''
+    124: HARD
+    The path sum of a path is the sum of the 
+    node's values in the path.
+    Given the root of a binary tree, return 
+    the maximum path sum of any non-empty path.
+    '''
+    def maxPathSum(self, root: TreeNode) -> int:
+        
+        # post order traversal
+        def subTreeGain(root):
+            nonlocal max_sum
+            if not root:
+                return 0
+            # gain from the left subtree
+            left_gain = max(subTreeGain(root.left), 0)
+            # gain from the right subtree
+            right_gain = max(subTreeGain(root.right), 0)
+            # track max, incase the max does not include the root but only one subtree
+            max_sum = max(left_gain + right_gain + root.val, max_sum)
+            # return gain when root is included
+            return max(left_gain, right_gain) + root.val
+
+        max_sum = -float('inf')
+        max_sum_root = subTreeGain(root)
+        return max_sum
---
+++ trees/serialize_and_deserialize_binary_tree.py
@@ -0,0 +1,58 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+    '''
+    297: HARD
+    Design an algorithm to serialize and deserialize a binary tree.
+    There is no restriction on how your serialization/deserialization
+    algorithm should work. You just need to ensure that a binary
+    tree can be serialized to a string and this string can be
+    deserialized to the original tree structure.
+    '''
+    # use preorder traveral
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        def rserialize(root, string):
+            if root is None:
+                string += 'None,'
+            else:
+                string += str(root.val) + ','
+                string = rserialize(root.left, string)
+                string = rserialize(root.right, string)
+            return string
+        
+        return rserialize(root, '')
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        def rdeserialize(l):
+            if l[0] == 'None':
+                l.pop(0)
+                return None
+            root = TreeNode(l[0])
+            l.pop(0)
+            root.left = rdeserialize(l)
+            root.right = rdeserialize(l)
+            return root
+
+        data_list = data.split(',')
+        root = rdeserialize(data_list)
+        return root
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
