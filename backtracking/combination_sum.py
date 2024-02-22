19b2efaf7d305a587fa33d6a3f70caa287875a0e
Dhruthick Gowda <dhruthick1215.m@gmail.com>
2024-02-22 12:54:28 -0800

    two backtracking problems

---
+++ backtracking/combination_sum.py
@@ -0,0 +1,28 @@
+class Solution:
+    '''
+    39: MEDIUM
+    Given an array of distinct integers candidates 
+    and a target integer target, return a list of 
+    all unique combinations of candidates where the 
+    chosen numbers sum to target. You may return 
+    the combinations in any order.
+    
+    The same number may be chosen from candidates
+    an unlimited number of times. Two combinations 
+    are unique if the frequency of at least one of 
+    the chosen numbers is different.
+    '''
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        def find_combinations(combination, sum_left, start):
+            if sum_left == 0:
+                nonlocal answer
+                answer.append(combination)
+                return
+            if sum_left < 0:
+                return
+            for i in range(start, len(candidates)):
+                find_combinations(combination + [candidates[i]], sum_left - candidates[i], i)
+        answer = []
+        find_combinations([], target, 0)
+
+        return answer
---
+++ backtracking/word_search.py
@@ -0,0 +1,39 @@
+class Solution:
+    '''
+    79: MEDIUM
+
+    Given an m x n grid of characters board and a string 
+    word, return true if word exists in the grid.
+    
+    The word can be constructed from letters of 
+    sequentially adjacent cells, where adjacent cells 
+    are horizontally or vertically neighboring. The same 
+    letter cell may not be used more than once.
+    '''
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        row_length, col_length = len(board), len(board[0])
+
+        
+        def find_word(row, col, letters_left):
+
+            if len(letters_left) == 0:
+                return True
+
+            if row < 0 or row == row_length or col < 0 or col == col_length \
+                    or board[row][col] != letters_left[0]:
+                return False
+
+            board[row][col] = '*'
+            for r_offset, c_offset in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                if find_word(row + r_offset, col + c_offset, letters_left[1:]):
+                    return True
+            board[row][col] = letters_left[0]
+
+            return False
+        
+
+        for row in range(row_length):
+            for col in range(col_length):
+                if find_word(row, col, word):
+                    return True
+        return False
