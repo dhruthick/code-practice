bdb70673fa9e6749deaffbc8809784e4b6bedeb9
Dhruthick Gowda <dhruthick1215.m@gmail.com>
2024-02-22 12:54:17 -0800

    one heap problem

---
+++ heaps/find_median_from_datastream.py
@@ -0,0 +1,28 @@
+'''
+295: HARD
+Implement the MedianFinder class:
+
+- MedianFinder() initializes the MedianFinder object.
+- void addNum(int num) adds the integer num from the 
+data stream to the data structure.
+- double findMedian() returns the median of all 
+elements so far. 
+'''
+
+import heapq
+class MedianFinder:
+
+    def __init__(self):
+        self.low = []
+        self.high = []        
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.low, - num)
+        heapq.heappush(self.high, - heapq.heappop(self.low))
+        if len(self.low) < len(self.high):
+            heapq.heappush(self.low, - heapq.heappop(self.high))
+
+    def findMedian(self) -> float:
+        if len(self.high) == len(self.low):
+            return (- self.low[0] + self.high[0])/2
+        return - self.low[0]
