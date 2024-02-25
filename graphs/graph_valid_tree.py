ce53c8655351aa9b9818b03c6ab0d6f0a254c0db
Dhruthick Gowda <dhruthick1215.m@gmail.com>
2024-02-25 13:25:48 -0800

    two graph problems

---
+++ graphs/graph_valid_tree.py
@@ -0,0 +1,52 @@
+class Solution(object):
+    '''
+    261: MEDIUM
+    
+    You have a graph of n nodes labeled from 
+    0 to n - 1. You are given an integer n 
+    and a list of edges where 
+    edges[i] = [ai, bi] indicates that there 
+    is an undirected edge between nodes ai 
+    and bi in the graph.
+
+    Return true if the edges of the given 
+    graph make up a valid tree, and false 
+    otherwise.
+    '''
+    def validTree(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: bool
+        """
+
+        from collections import defaultdict
+        adj_list = defaultdict(list)
+        for u, v in edges:
+            adj_list[u].append(v)
+            adj_list[v].append(u)
+        
+        visited = set() # to check connectivity
+        tracked = set() # to check for cycles
+        def dfs(node):
+            # loop detected
+            if node not in visited: visited.add(node)
+            if node in tracked:
+                return False
+            # no pre-reqs
+            if not adj_list[node]:
+                return True
+            
+            tracked.add(node)
+            # recursively explore each neighbouting path.
+            for n in adj_list[node]:
+                # extra condition because the graph is undirected
+                if node in adj_list[n]: 
+                    adj_list[n].remove(node)
+                if not dfs(n): 
+                    return False
+            tracked.remove(node)
+
+            return True
+
+        return dfs(0) and len(visited) == n
---
+++ graphs/no_of_connected_components_in _undirected_graph.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    '''
+    323: MEDIUM
+    You have a graph of n nodes. You are given an 
+    integer n and an array edges where 
+    edges[i] = [ai, bi] indicates that there is 
+    an edge between ai and bi in the graph.
+
+    Return the number of connected components in 
+    the graph.
+    '''
+    def countComponents(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: int
+        """
+        from collections import defaultdict
+
+        visited = set()
+
+        adj_list = defaultdict(list)
+        for u, v in edges:
+            adj_list[u].append(v)
+            adj_list[v].append(u)
+
+        def bfs(root):
+            q = [root]
+            while q:
+                node = q.pop(0)
+                if node not in visited:
+                    visited.add(node)
+                    q.extend(adj_list[node])
+        
+        n_comp = 0
+        for node in range(n):
+            if node not in visited:
+                n_comp += 1
+                bfs(node)
+        return n_comp
